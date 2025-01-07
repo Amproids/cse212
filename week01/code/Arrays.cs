b83e7e561f10fb5247e7124dc72f8c137039808d
Andrew Parry <parryandrew47@gmail.com>
2025-01-06 23:53:05 -0700

    adding code for arrays for week 01

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -9,11 +9,16 @@ public static class Arrays
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // Create empty integer array, witht the size of length
+        // Create a for loop to iterate {length} number of times
+        // multiply the number by the iteration + 1 and put into array
+        //after the loop return the array
 
-        return []; // replace this return statement with your own
+        var multiplesArray = new double[length];
+        for (int i = 0; i < length; i++) {
+            multiplesArray[i] = number * (i + 1);
+        }
+        return multiplesArray;
     }
 
     /// <summary>
@@ -26,8 +31,23 @@ public static class Arrays
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // create empty array, for final list
+        // if the amount rotated is larger than the array, we can just mod the amount by the length of the array
+        // in two for loops, add in the values from the initial array
+        // first, add ammount number of values, starting at list length - amount - 1 and for ammount number of iterations
+        // second add list length - ammount number of values, starting at 0 and iterating to length - ammount - 1
+        // return final list array
+
+        int dataLength = data.Count;
+        amount = amount % dataLength;
+        var rotatedList = new List<int>();
+        for (int i = dataLength - amount; i < dataLength; i++) {
+            rotatedList.Add(data[i]);
+        }
+        for (int i = 0; i < dataLength - amount; i++) {
+            rotatedList.Add(data[i]);
+        }
+        data.Clear();
+        data.AddRange(rotatedList);
     }
 }
